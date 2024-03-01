e2fdcd6609be8f8012d29c79f67f9b92e18ca79f
Russell Corbett <87535812+Foul-Trouble@users.noreply.github.com>
2024-03-01 02:49:14 -0330

    Modified community column
    Added Copper themeing

--- frontend/app/components/MainLayout.tsx
+++ frontend/app/components/MainLayout.tsx
@@ -4,10 +4,17 @@ import { UserService } from "~/services/services";
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import { useNavigate } from '@remix-run/react';
+import { keyframes } from '@emotion/react';
 
 interface MainLayoutProps {
 	children: React.ReactNode;
 }
+
+const heat = keyframes`
+	0% { box-shadow: 0 0 10px #B87333; }
+	50% { box-shadow: 0 0 15px #FF4500; }
+	100% { box-shadow: 0 0 20px #B22222; }
+`;
 
 export function MainLayout(props: MainLayoutProps) {
 	const [communities, setCommunities] = useState([]);
@@ -22,7 +29,7 @@ export function MainLayout(props: MainLayoutProps) {
 
 	return (
 		<>
-			<Box w='5vw' h='100vh' justifyItems='center' bgColor='#FF6600' borderRight='1px' borderColor='gray'>
+			<Box w='5vw' h='100vh' justifyItems='center' style={{background: 'linear-gradient(to right, #B87333, white, #B87333)'}} borderRight='1px' borderColor='gray'>
 				<Flex alignItems='center' gap={3} direction='column' h='100%' pt={3}>
 					<Button 
 						w={14} 
@@ -35,14 +42,21 @@ export function MainLayout(props: MainLayoutProps) {
 					</Button>
 					{communities.map((community: any, index: number) => (
 						<Button
-							key={index}
-							w={14}
-							h={14}
-							onClick={() => {
-								navigate(`/community/${community.id}`)
-							}}
+						key={index}
+						w={14}
+						h={14}
+						borderColor={'gray'}
+						borderWidth={3}
+						_hover={{
+							transform: "scale(1.10)",
+							animation: `${heat} 10s linear forwards`
+						}}		
+						onClick={() => {
+							navigate(`/community/${community.id}`)
+						}}
+						transition="all 0.5s ease-out"
 						>
-							{community.name.charAt(0)}
+						{community.name.charAt(0)}
 						</Button>
 					))}
 				</Flex>
