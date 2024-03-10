bb82a16383ea7eb53cd5352a9b51866a6ba3a0e7
Russell Corbett <russellcorbett11@gmail.com>
2024-03-10 16:58:03 -0230

    Update with Prettier and new line

--- frontend/app/components/FeedSelector.tsx
+++ frontend/app/components/FeedSelector.tsx
@@ -13,13 +13,15 @@ export function FeedSelector(props: FeedSelectorProps) {
 	const matches = useMatches();
 
 	// Get the active button index from the current URL
-	const activeButton = matches.findIndex(match => match.pathname.includes('/posts')) !== -1 ? null : props.channels.findIndex(channel => matches.some(match => match.pathname.includes(channel.id)));
+	const activeButton = matches.findIndex((match) => match.pathname.includes("/posts")) !== -1 ? null : props.channels.findIndex((channel) => matches.some((match) => match.pathname.includes(channel.id)));
 
 	return (
-		<Box minW='17rem' h='100vh' justifyItems='center' bgColor={colours['channel-bar']} borderRight='1px' borderColor='gray' boxShadow='2xl'>
-			<Flex alignItems='center' gap={2} direction='column' h='100%' pt={3}>
-				<Flex direction='row'>
-					<Text fontSize={18} fontWeight='bold'>{(props.community as { name: string }).name}</Text>
+		<Box minW="17rem" h="100vh" justifyItems="center" bgColor={colours["channel-bar"]} borderRight="1px" borderColor="gray" boxShadow="2xl">
+			<Flex alignItems="center" gap={2} direction="column" h="100%" pt={3}>
+				<Flex direction="row">
+					<Text fontSize={18} fontWeight="bold">
+						{(props.community as { name: string }).name}
+					</Text>
 				</Flex>
 				<Button
 					w={236}
@@ -32,14 +34,13 @@ export function FeedSelector(props: FeedSelectorProps) {
 									borderRadius: "10px",
 									background: "linear-gradient(315deg, #ffffff, #e6e6e6)",
 									boxShadow: "-3px -3px 6px #666666, 3px 3px 6px #ffffff",
-							}
+							  }
 							: {
 									borderRadius: "10px",
 									background: "linear-gradient(315deg, #ffffffa, #e6e6e6)",
 									boxShadow: "none",
-							}
-					}
-				>
+							  }
+					}>
 					Posts
 				</Button>
 				<Text fontSize={18}>Channels</Text>
@@ -56,12 +57,12 @@ export function FeedSelector(props: FeedSelectorProps) {
 										borderRadius: "10px",
 										background: "linear-gradient(315deg, #ffffff, #e6e6e6)",
 										boxShadow: "-3px -3px 6px #666666, 3px 3px 6px #ffffff",
-								}
+								  }
 								: {
 										borderRadius: "10px",
 										background: "linear-gradient(315deg, #ffffffa, #e6e6e6)",
 										boxShadow: "none",
-								}
+								  }
 						}>
 						#{channel.name}
 					</Button>
