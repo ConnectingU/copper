7c87c277be249d441cae5be17132f917f085c8fb
liam-earle <38230039+liam-earle@users.noreply.github.com>
2024-03-23 21:19:40 -0230

    missing files

---
+++ backend/api/endpoints/history/services/history.service.ts
@@ -0,0 +1,71 @@
+import { Service } from 'typedi';
+import { db } from '../../../app';
+
+@Service()
+export class HistoryService {
+	constructor() {}
+
+	async read(req) {
+		const userId: number = +req.params.userId;
+		const channelId: number = +req.params.channelId;
+
+		const history = await db.history.findFirst({
+			where: { userId, channelId },
+			select: {
+				lastVisited: true,
+			}
+		});
+
+		const messages = await db.message.findMany({
+			where: { channelId },
+			select: {
+				createdAt: true,
+			}
+		});
+
+		let count = 0;
+
+		messages.forEach(element => {
+			if (element.createdAt > history.lastVisited) {
+				count++;
+			}
+		});
+
+		return {count};
+	}
+	
+	async update(req) {
+		const userId: number = +req.params.userId;
+		const channelId: number = +req.params.channelId;
+		const { lastVisited } = req.body;
+
+		const history = await db.history.findFirst({
+			where: { userId, channelId },
+			select: {
+				id: true,
+				userId: true,
+				channelId: true,
+				lastVisited: true,
+			}
+		});
+
+		if (!history) {
+			const newHistory = await db.history.create({
+				data: {
+					userId,
+					channelId,
+					lastVisited,
+				}
+			});
+			return newHistory; 
+		}
+
+		const updatedHistory = db.history.update({
+			where: { id: history.id },
+			data: {
+				lastVisited
+			}
+		});
+		return updatedHistory
+	}
+}
---
+++ frontend/app/services/history.service.ts
@@ -0,0 +1,40 @@
+import Cookies from 'js-cookie';
+import { Service } from "./service";
+
+class HistoryService extends Service {
+	async getHistory(userId: number, channelId: number) {
+		const config = {
+			headers: {
+				'Authorization': `Bearer ${Cookies.get('auth')}`,
+			}
+		};
+
+		try {
+			const resp = await this.http.get(`${this.baseURL}/history/${userId}/${channelId}`, config);
+			return resp.data;
+		} catch(error) {
+			console.error(error);
+			throw error;
+		}
+	}
+
+	async updateHistory(userId: number, channelId: number, lastVisited: Date) {
+		const body = {lastVisited};
+		const config = {
+			headers: {
+				'Authorization': `Bearer ${Cookies.get('auth')}`,
+			}
+		};
+
+		try {
+			const resp = await this.http.patch(`${this.baseURL}/history/${userId}/${channelId}`, body, config);
+			return resp.data;
+		} catch(error) {
+			console.error(error);
+			throw error;
+		}
+	}
+}
+
+const historyService = new HistoryService();
+export default historyService as HistoryService;
